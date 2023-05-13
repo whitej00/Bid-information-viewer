6f428d0d57afa260103c3a9c722bdc5714e5587e
whitej00 <68831140+whitej00@users.noreply.github.com>
2023-05-13 12:07:55 +0900

    Update settings.py

--- searchbid/settings.py
+++ searchbid/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = 'e!2_#jfd5ze7kfdr$f%vm_p6oc*pd142x(!#xhf(kzhh=tqf$9'
+SECRET_KEY = 'wtf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
